3ce36446c26cc1d3c3bcadc5a0156fe2047c8736
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2023-01-19 15:32:31 +0530

    Added: Editing Existing Contact

--- src/com/bridgelabz/addressbook/AddressBook.java
+++ src/com/bridgelabz/addressbook/AddressBook.java
@@ -1,36 +1,88 @@
 package com.bridgelabz.addressbook;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AddressBook {
 
-	Scanner scanner = new Scanner(System.in);
+	//Creating instance object
+		Scanner scanner = new Scanner(System.in);
+		ContactPerson contact = new ContactPerson();
+		ArrayList<ContactPerson> contactsArrayList = new ArrayList<ContactPerson>();
+		
+		/*
+		 * Adding contacts
+		 */
+		public void addContact() {
+			ContactPerson contact = new ContactPerson();
+			
+			System.out.println("Enter your FirstName: ");
+			contact.setFirstName(scanner.next());
+			System.out.println("Enter your LastName: ");
+			contact.setLastName(scanner.next());
+			System.out.println("Enter your Email: ");
+			contact.setEmail(scanner.next());
+			System.out.println("Enter your Address: ");
+			contact.setAddress(scanner.next());
+			System.out.println("Enter your City: ");
+			contact.setCity(scanner.next());
+			System.out.println("Enter your State: ");
+			contact.setState(scanner.next());
+			System.out.println("Enter your Zipcode: ");
+			contact.setZip(scanner.nextInt());
+			System.out.println("Enter your Mobile Number: ");
+			contact.setMobile(scanner.nextLong());
+			
+			contactsArrayList.add(contact);
+		}
+		/*
+		 * Print out on all contacts using method contact class
+		 */
+		public void showContacts() {
+			System.out.println(contact.toString());
+			for (int i = 0; i < contactsArrayList.size(); i++) {
+				ContactPerson contacts = contactsArrayList.get(i);
+				System.out.println(contacts.toString());
+			}
+		}
+		
+		public void editContact() {
+			/*
+			 * Ability to edit existing contact person using their
+			 */
+			System.out.println("Enter the First Name to Edit : ");
+			String FirstName = (scanner.next());
+			boolean IsAvaible = false;
 
-	ContactPerson contact = new ContactPerson(); // create a ContactPerson object
+			for (ContactPerson contact : contactsArrayList) {
+				if (contact.getFirstName().equals(FirstName)) {
+					IsAvaible = true;
 
-	// Add contacts
+					System.out.println("Enter your FirstName: ");
+					contact.setFirstName(scanner.next());
+					System.out.println("Enter your LastName: ");
+					contact.setLastName(scanner.next());
+					System.out.println("Enter your Email: ");
+					contact.setEmail(scanner.next());
+					System.out.println("Enter your Address: ");
+					contact.setAddress(scanner.next());
+					System.out.println("Enter your City: ");
+					contact.setCity(scanner.next());
+					System.out.println("Enter your State: ");
+					contact.setState(scanner.next());
+					System.out.println("Enter your Zipcode: ");
+					contact.setZip(scanner.nextInt());
+					System.out.println("Enter your Mobile Number: ");
+					contact.setMobile(scanner.nextLong());
+				}
+			}
+			/*
+			 * If the condition false to have printed conatct doesnt exist
+			 */
+			if (IsAvaible == false) {
+				System.out.println("Contact Doesn't exist.");
+			}
+		
+		}
 
-	public void addContact() {
-		System.out.println("Enter your FirstName: ");
-		contact.setFirstName(scanner.next());
-		System.out.println("Enter your LastName: ");
-		contact.setLastName(scanner.next());
-		System.out.println("Enter your Email: ");
-		contact.setEmail(scanner.next());
-		System.out.println("Enter your Address: ");
-		contact.setAddress(scanner.next());
-		System.out.println("Enter your City: ");
-		contact.setCity(scanner.next());
-		System.out.println("Enter your State: ");
-		contact.setState(scanner.next());
-		System.out.println("Enter your Zipcode: ");
-		contact.setZip(scanner.nextInt());
-		System.out.println("Enter your Mobile Number: ");
-		contact.setMobile(scanner.nextLong());
 	}
-
-	public void showContacts() {
-		System.out.println(contact.toString());
-	}
-
-}
--- src/com/bridgelabz/addressbook/AddressBookMain.java
+++ src/com/bridgelabz/addressbook/AddressBookMain.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.addressbook;
 
+
 import java.util.Scanner;
 
 public class AddressBookMain {
@@ -8,34 +9,35 @@ public class AddressBookMain {
 		System.out.println("Welcome To Address Book"); // print welcome massage
 		AddressBook addressBook = new AddressBook();
 		Scanner scanner = new Scanner(System.in); // Create object
-		// initalize
+		// Initialize
 		int Number;
 
 		/**
-		 * Enter number 1 to add this contacts details 
-		 * Enter number 2 to displaying
-		 * contact details
+		 * Enter number 1 to add this contacts details Enter number 2 to displaying
+		 * contact details Enter number 3 to Editing contacts details
 		 */
 		do {
-			System.out.println(" 1. Add Contact \n 2. Display contact \n");
-			System.out.println("Enter the  Number: ");
-
-			Number = scanner.nextInt(); // Reading values from user
+			System.out.println("1. Add Contact \n2. Display contact \n3.EDIT CONTACT");
+			System.out.println("Enter the Number: ");
+			Number = scanner.nextInt();
 
 			switch (Number) {
+
 			case 1:
 				addressBook.addContact();
 				break;
 			case 2:
 				addressBook.showContacts();
 				break;
+			case 3:
+				addressBook.editContact();
+				break;
 			default:
 				System.out.println(" !!!Invalid Input!!! ");
 				break;
 
 			}
-		} while (Number != 3); // calling
-
+		} while (Number < 4); // calling
 	}
 
 }
