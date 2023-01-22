8c4400648682312c48e3a7ef03724be37f69c3b1
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2023-01-22 17:11:32 +0530

    Added:Add multiple person

--- src/com/bridgelabz/addressbook/AddressBookMain.java
+++ src/com/bridgelabz/addressbook/AddressBookMain.java
@@ -11,14 +11,15 @@ public class AddressBookMain {
 		// initalize
 		int Number;
 
-		/**
-		 * Enter number 1 to add this contacts details Enter number 2 to displaying
-		 * contact details Enter number 3 to Editing contacts details Enter number 4 to
-		 * Delete contact
-		 */
+		 /**
+         * Enter number 1 to add this contacts details
+         * Enter number 2 to displaying contact details
+         * Enter number 3 to Editing contacts details
+         * Enter number 4 to Delete contact
+         */
 		do {
-			System.out.println(" 1. Add Contact \n2. Display contact \n3.EDIT CONTACT \n4.DELETE CONTACT \n");
-			System.out.println("~Enter the Number~: ");
+			System.out.println("1. Add Contact \n2. Display contact \n3. EDIT CONTACT \n4. DELETE CONTACT \n");
+			System.out.println("Enter the Number: ");
 			Number = scanner.nextInt();
 
 			switch (Number) {
@@ -36,11 +37,11 @@ public class AddressBookMain {
 				addressBook.deleteContact();
 				break;
 			default:
-				System.out.println(" !!!Invalid Input!!! ");
+				System.out.println(" Invalid Input ");
 				break;
 
 			}
-		} while (Number < 4); // calling
+		} while (Number != 4); // calling
 	}
 
 }
